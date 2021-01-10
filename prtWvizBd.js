45652f88704b560c3394210fa5a85b0451e86dff
54AP5vxvkpQdST <77215790+54AP5vxvkpQdST@users.noreply.github.com>
2021-01-10 03:31:30 +0200

    Update prtWvizBd.js

--- prtWvizBd.js
+++ prtWvizBd.js
@@ -1,2 +1,27 @@
+'use strict';
+
+const e = React.createElement;
+
+class SePrtWvizBd extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+
+
 const domContainer = document.querySelector('#prtWvizBd');
 ReactDOM.render(e(SePrtWvizBd), domContainer);
