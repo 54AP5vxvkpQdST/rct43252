ee309e9f0567e1db103fe58b5d07491ea11e3793
54AP5vxvkpQdST <77215790+54AP5vxvkpQdST@users.noreply.github.com>
2021-01-10 03:57:54 +0200

    Update prtWvizBd.js

--- prtWvizBd.js
+++ prtWvizBd.js
@@ -1,24 +1,54 @@
 'use strict';
 
-const e = React.createElement;
+//         const e = React.createElement;
 
-class SePrtWvizBd extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+// class SePrtWvizBd extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
 
-  render() {
-    if (this.state.liked) {
-      return 'eleven';
+//   render() {
+//     if (this.state.liked) {
+//       return 'eleven';
+//     }
+
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'one plus one'
+//     );
+//   }
+// }
+
+const Component = () => {
+  useEffect(() => {
+    window.addEventListener('beforeunload', alertUser)
+    window.addEventListener('unload', handleEndConcert)
+    return () => {
+      window.removeEventListener('beforeunload', alertUser)
+      window.removeEventListener('unload', handleEndConcert)
+      handleEndConcert()
     }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'one plus one'
-    );
+  }, [])
+  const alertUser = e => {
+    e.preventDefault()
+    e.returnValue = ''
   }
+  const handleEndConcert = async () => {
+    await fetcher({
+      url: endConcert(concert.id),
+      method: 'PUT'
+    })
+  }
+  return (
+    <Container>
+      <Prompt
+        when={isPrompt()}
+        message={() => 'Are you sure you want to leave this page?'}
+      />
+    </Container>
+  )
 }
 
 
